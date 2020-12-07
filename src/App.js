edefbd5e63f9fe9604036c86c238348c3ec26a15
Macrichie <makanjuolakunle@gmail.com>
2020-12-07 11:18:58 +0000

    Initial commit: added app directories

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { RestaurantsContextProvider } from "./contextAPI/RestaurantsContext";
+import Home from "./routes/Home";
+import RestaurantsDetailsPage from "./routes/RestaurantsDetailsPage";
+import UpdatePage from "./routes/UpdatePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/restaurants/:id"
+              component={RestaurantsDetailsPage}
+            />
+            <Route
+              exact
+              path="/restaurants/:id/update"
+              component={UpdatePage}
+            />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
   );
 }
 
